44ab440362ab091a9dd80f0aca3ad9eb049cdfcb
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-29 16:12:13 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="h-screen  w-screen flex justify-center items-center">
+      <p className="text-3xl text-center font-semibold">
+        This site is still under development
+      </p>
+    </div>
+  );
+};
+
+export default App;
